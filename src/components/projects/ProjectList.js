17c813f9d69f211f97fee872a5086d833cb08fa7
Bander <bannadr1@hotmail.com>
2019-05-10 13:47:38 +0300

    project summary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,35 +1,16 @@
 import React from 'react'
-
+import ProjectSummary from './ProjectSummary'
 const ProjectList = () => {
-  return (
-    <div className="project-list section">
+    return (
+        <div className="project-list section">
 
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by Bander</p>
-            <p className="grey-text">3rd Sep, 2am</p>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+
         </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by Bander</p>
-            <p className="grey-text">3rd Sep, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by Bander</p>
-            <p className="grey-text">3rd Sep, 2am</p>
-        </div>
-      </div>
-
-    </div>
-  )
+    )
 }
 
 export default ProjectList
