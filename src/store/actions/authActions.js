59abd380773e61fb74f657765d1857cf46091140
Bander <bannadr1@hotmail.com>
2019-05-11 14:41:53 +0300

    logout

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -12,3 +12,17 @@ export const signIn = credentials => {
       });
   };
 };
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({type: 'SIGNOUT_SUCCESS', })
+      })
+      .catch(error => {
+        dispatch({type: 'SIGNOUT_FAILED', })
+      });
+  };
+};
